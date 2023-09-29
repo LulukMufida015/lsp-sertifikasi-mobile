7e1e48932797b644fa068fb10a768850446f05bd
LulukMufida015 <lulukmufida0015@gmail.com>
2023-09-30 05:37:16 +0700

    sertifikasi-mobile

---
+++ src/login/index.jsx
@@ -0,0 +1,101 @@
+import React, { useState } from 'react';
+import { View, Text, Image, TextInput, TouchableOpacity, StyleSheet } from 'react-native';
+import db from '../config/db';
+
+function LoginScreen({ navigation }) {
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+    const handleLogin = () => {
+        db.transaction((tx) => {
+            // SELECT query untuk memeriksa apakah username dan password cocok
+            tx.executeSql(
+                'SELECT * FROM users WHERE username = ? AND password = ?;',
+                [username, password],
+                (tx, results) => {
+                    if (results.rows.length > 0) {
+                        alert('Login berhasil!');
+                        navigation.navigate('Home'); // Navigasi ke halaman Home setelah login berhasil
+                    } else {
+                        alert('Username atau password salah. Silakan coba lagi.');
+                    }
+                },
+                (error) => {
+                    alert('Terjadi kesalahan: ' + error);
+                }
+            );
+        });
+    };
+
+    return (
+        <View style={styles.container}>
+            <Image
+                source={require('../assets/Logo.png')}
+                style={styles.logo}
+            />
+            <Text style={styles.appTitle}>MyCashBook</Text>
+            <TextInput
+                style={styles.input}
+                placeholder="Username"
+                value={username}
+                onChangeText={(text) => setUsername(text)}
+            />
+            <TextInput
+                style={styles.input}
+                placeholder="Password"
+                secureTextEntry
+                value={password}
+                onChangeText={(text) => setPassword(text)}
+            />
+            <View style={{ width: '80%' }}>
+                <TouchableOpacity style={styles.loginButton} onPress={handleLogin}>
+                    <Text style={styles.buttonText}>Login</Text>
+                </TouchableOpacity>
+
+                <TouchableOpacity style={[{ marginTop: 10 }]}>
+                    <Text>Belum punya akun? <TouchableOpacity onPress={() => navigation.navigate('Register')}><Text style={{ fontWeight: 'bold' }}>Daftar</Text></TouchableOpacity></Text>
+                </TouchableOpacity>
+            </View>
+        </View>
+    );
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: '#fff',
+    },
+    logo: {
+        width: 100,
+        height: 100,
+        marginBottom: 20,
+    },
+    appTitle: {
+        fontSize: 24,
+        fontWeight: 'bold',
+        marginBottom: 20,
+    },
+    input: {
+        width: '80%',
+        height: 40,
+        borderColor: 'gray',
+        borderWidth: 1,
+        borderRadius: 5,
+        marginBottom: 10,
+        paddingHorizontal: 10,
+    },
+    loginButton: {
+        backgroundColor: 'orange',
+        padding: 10,
+        borderRadius: 5,
+    },
+    buttonText: {
+        color: 'white',
+        fontSize: 16,
+        fontWeight: 'bold',
+        textAlign: 'center'
+    },
+});
+
+export default LoginScreen;
---
+++ src/register/index.jsx
@@ -0,0 +1,98 @@
+import React, { useState } from 'react';
+import { View, Text, TextInput, TouchableOpacity, StyleSheet } from 'react-native';
+import db from '../config/db';
+
+function RegisterScreen({ navigation }) {
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+
+    const handleRegister = () => {
+        db.transaction((tx) => {
+            // Lakukan INSERT data pengguna ke dalam tabel users
+            tx.executeSql(
+                'INSERT INTO users (username, password) VALUES (?, ?);',
+                [username, password],
+                (tx, results) => {
+                    if (results.rowsAffected > 0) {
+                        alert('Registrasi berhasil!');
+                        navigation.navigate('Login'); // Navigasi ke halaman login setelah registrasi
+                    } else {
+                        alert('Registrasi gagal. Silakan coba lagi.');
+                    }
+                },
+                (error) => {
+                    alert('Terjadi kesalahan: ' + error);
+                }
+            );
+        });
+    };
+
+    return (
+        <View style={styles.container}>
+            <Text style={styles.title}>Register</Text>
+
+            <Text style={styles.label}>Username:</Text>
+            <TextInput
+                style={styles.input}
+                placeholder="Masukkan username"
+                value={username}
+                onChangeText={(text) => setUsername(text)}
+            />
+
+            <Text style={styles.label}>Password:</Text>
+            <TextInput
+                style={styles.input}
+                placeholder="Masukkan password"
+                secureTextEntry
+                value={password}
+                onChangeText={(text) => setPassword(text)}
+            />
+
+            <TouchableOpacity style={styles.button} onPress={handleRegister}>
+                <Text style={styles.buttonText}>Daftar</Text>
+            </TouchableOpacity>
+        </View>
+    );
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: '#fff',
+    },
+    title: {
+        fontSize: 20,
+        fontWeight: 'bold',
+        marginBottom: 20,
+    },
+    label: {
+        fontSize: 16,
+        fontWeight: 'bold',
+        marginBottom: 10,
+    },
+    input: {
+        width: '80%',
+        height: 40,
+        borderColor: 'gray',
+        borderWidth: 1,
+        borderRadius: 5,
+        marginBottom: 10,
+        paddingHorizontal: 10,
+    },
+    button: {
+        backgroundColor: 'green',
+        padding: 10,
+        borderRadius: 5,
+        marginTop: 20,
+    },
+    buttonText: {
+        color: 'white',
+        fontSize: 16,
+        fontWeight: 'bold',
+        textAlign: 'center',
+    },
+});
+
+export default RegisterScreen;
